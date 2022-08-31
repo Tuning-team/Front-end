2c40cd2873ca86ea1ca0181e55c961d035652869
MinjiJeon <minji799@gmail.com>
2022-08-31 10:33:29 +0900

    [edit]tdd통과완료

--- src/test/collectionList.test.js
+++ src/test/collectionList.test.js
@@ -2,18 +2,48 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CollectionList from "../common/CollectionList";
 
-// test("테스트", () => {
-//   render(<CollectionList />);
-//   const { getByText } = render(<CollectionList />);
-//   const count = screen.queryByText("내 컬랙션");
-//   expect(count).toBeVisible();
-// });
-
-//! ui테스트
 describe("<CollectionList />", () => {
-  it("잘되나", () => {
+  const state = [
+    {
+      collectionTitle: "우울할때 보는영상",
+      id: 0,
+      likes: 3,
+      comments: 23,
+      thumbnail: [
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+      ],
+    },
+    {
+      collectionTitle: "배고플때 보는영상",
+      id: 1,
+      likes: 3,
+      comments: 23,
+      thumbnail: [
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+      ],
+    },
+    {
+      collectionTitle: "웃긴영상 모음집",
+      id: 2,
+      likes: 5,
+      comments: 21,
+      thumbnail: [
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+        "https://via.placeholder.com/150x150",
+      ],
+    },
+  ];
+
+  it("컬렉션리스트 ui테스트", () => {
     const { getByText } = render(<CollectionList />);
-    getByText("내 컬랙션");
     getByText("웃긴영상 모음집");
     getByText("좋아요 5");
     getByText("댓글 21"); // button이 있는지 확인
