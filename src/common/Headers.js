6a0252996e76b42f47d78782411d98ba82f93141
jhhyung <jinhahyung@gmail.com>
2022-09-01 17:11:04 +0900

    [update]header의 로그인 버튼에 global button style 적용

--- src/common/Headers.js
+++ src/common/Headers.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Button } from "../elements/Button";
 
 const Headers = () => {
   const nav = useNavigate();
   return (
     <Header>
       <div>Tuning</div>
-      <button onClick={() => nav("/login")}>로그인</button>
+      <Button onClick={() => nav("/login")}>로그인</Button>
     </Header>
   );
 };
