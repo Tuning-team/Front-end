8ad1deb041eee8e5a9fa9e4b312ec87a473bd7cb
Dopest-HYO <hyo01_@naver.com>
2022-09-08 07:27:51 +0900

    [edit]Comment API통신을 위해 collectionId 받아오도록 코드 수정./ [update] Search 검색값 입력 후 response 잘 받아오는 것을 확인. 추후 map으로 결과 보여주기 작업 남음

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import { myCollectionSlice } from "./modules/collectionSlice";
 import { collectionSlice } from "./modules/tempCollectionSlice";
 import { commentSlice } from "./modules/commentSlice";
 import { videoSlice } from "./modules/videoSlice";
+import { searchSlice } from "./modules/searchSlice";
 
 export default configureStore({
   reducer: {
     myCollectionSlice: myCollectionSlice.reducer,
     collectionSlice: collectionSlice.reducer,
     commentSlice: commentSlice.reducer,
+    searchSlice: searchSlice.reducer,
     videoSlice: videoSlice.reducer,
   },
   devTools: process.env.NODE_ENV !== "development",
