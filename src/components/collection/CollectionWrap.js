c6d57390b96c42ce256d786757d0eeb639a79de9
jhhyung <jinhahyung@gmail.com>
2022-09-07 09:53:01 +0900

    [update]컬렉션 페이지에 대표 이미지 추가

--- src/components/collection/CollectionWrap.js
+++ src/components/collection/CollectionWrap.js
@@ -1,26 +1,23 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getCollection } from "../../redux/modules/tempCollectionSlice";
+
 import CollectionInformation from "./CollectionInformation";
 import CollectionVideoList from "./CollectionVideoList";
 
+import styled from "styled-components";
+
 const CollectionWrap = () => {
   const param = useParams();
-
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.collectionSlice.data);
-
-  useEffect(() => {
-    dispatch(getCollection(param.collection_id));
-  }, []);
-
   return (
-    <>
-      <CollectionInformation data={data} collectionId={param.collection_id} />
+    <Layout>
+      <CollectionInformation collectionId={param.collection_id} />
       <CollectionVideoList collectionId={param.collection_id} />
       {/* <CollectionComment /> */}
-    </>
+    </Layout>
   );
 };
 export default CollectionWrap;
+const Layout = styled.div`
+  overflow-x: hidden;
+  margin: 0.6rem;
+`;
