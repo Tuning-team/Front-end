adaef20f601daee9daac94a4434b209440c3c948
Dopest-HYO <hyo01_@naver.com>
2022-09-09 12:44:40 +0900

    [edit] Search filter 수정 및 limit 갯수 확장(50)

--- src/components/search/SearchWrap.js
+++ src/components/search/SearchWrap.js
@@ -35,12 +35,13 @@ const SearchWrap = () => {
           {/* map 쓰실 때 참고하쎄용~ */}
           {/* map((data) => {"배고팡"})<- 괄호 들어가면 반드시 결과에 return */}
           {/* map((data) => "배고팡")<- 괄호 안 들어가면 자동으로 return 적용 */}
-          {searchList.map((data) => {
-            return <div key={data._id}>
-              <li></li>
-              <p>{data.collectionTitle}</p>
-            </div>
-          })}
+          {searchList.filter(data => data.collectionTitle.indexOf(search) !== -1
+            || data.description.indexOf(search) !== -1).map((data) => {
+              return <div key={data._id}>
+                <li></li>
+                <p>{data.collectionTitle}</p>
+              </div>
+            })}
         </ul>
       </div>
     </>
