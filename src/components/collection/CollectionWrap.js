87f59977a684f2209c62697590ec24c774b3c987
jhhyung <jinhahyung@gmail.com>
2022-09-14 02:35:31 +0900

    [update]영상/댓글 탭 메뉴 로직 추가

--- src/components/collection/CollectionWrap.js
+++ src/components/collection/CollectionWrap.js
@@ -12,20 +12,79 @@ const CollectionWrap = () => {
   const param = useParams();
   const nav = useNavigate();
 
+  const [tabClicked, setTabClicked] = useState(false);
+
+  const onClickedVideoTab = () => {
+    setTabClicked(false);
+  };
+  const onClickedCommentTab = () => {
+    setTabClicked(true);
+  };
+
   return (
-    <Layout>
-      <button onClick={() => nav(-1)}>뒤로가기</button>
+    <>
       <CollectionInformation collectionId={param.collection_id} />
-      <CollectionVideoList collectionId={param.collection_id} />
+      <TabMenu tabClicked={tabClicked}>
+        <div className="videoTab">
+          <span onClick={onClickedVideoTab}>영상</span>
+        </div>
+        <div className="commentTab">
+          <span onClick={onClickedCommentTab}>댓글</span>
+        </div>
+      </TabMenu>
+      {!tabClicked ? (
+        <CollectionVideoList collectionId={param.collection_id} />
+      ) : (
+        <>
+          <CommentForm collectionId={param.collection_id} />
+          {/* <CommentList collectionId={param.collection_id} /> */}
+        </>
+      )}
 
       {/* Comment Component 작업 중 */}
-      <CommentForm collectionId={param.collection_id} />
+      {/* <CommentForm collectionId={param.collection_id} /> */}
       {/* <CommentList collectionId={param.collection_id} /> */}
-    </Layout>
+    </>
   );
 };
 export default CollectionWrap;
-const Layout = styled.div`
-  overflow-x: hidden;
-  /* margin: 0.6rem; */
+
+const TabMenu = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: space-evenly;
+  border-bottom: 0.235rem solid #eaeaea;
+  padding: 0 1rem;
+  height: 2.5rem;
+
+  & > div {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & .videoTab {
+    border-bottom: 0.235rem solid
+      ${(props) => (props.tabClicked ? "transparent" : "#b295e9")};
+  }
+  & .commentTab {
+    border-bottom: 0.235rem solid
+      ${(props) => (props.tabClicked ? "#b295e9" : "transparent")};
+  }
+
+  & span {
+    display: inline-block;
+
+    font-size: 0.875rem;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.24;
+    letter-spacing: normal;
+  }
 `;
