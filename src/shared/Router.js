13303b9280cf14a193d3ba8df6622838c89131dd
jhhyung <jinhahyung@gmail.com>
2022-09-16 02:12:16 +0900

    [edit]사용하지 않는 페이지 코드(카테고리) 삭제

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Category from "../pages/CategoryPage";
 import LoginSignupPage from "../pages/LoginSignupPage";
 import MainPage from "../pages/MainPage";
 import MyCollectionPage from "../pages/MyCollectionPage";
@@ -18,12 +17,11 @@ function Router() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginSignupPage />} />
-        <Route path="/category" element={<Category />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/mypage" element={<MyCollectionPage />} />
         <Route path="/mypage/add" element={<AddCollectionPage />} />
         <Route path="/mypage/add/search" element={<AddVideoSearchPage />} />
-        <Route path="/Comment" element={<CommentPage />} />
+        <Route path="/comment" element={<CommentPage />} />
         <Route path="/collection/:collection_id" element={<CollectionPage />} />
         <Route path="/category/:collection_id" element={<CategoryPage />} />
         <Route path="/google_login/:token" element={<GoogleLogin />} />
