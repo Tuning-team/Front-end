3d6761a9c20818b6cf48a390534d9b019fc35bda
Hyo <0612hyo@gmail.com>
2022-09-19 17:35:38 +0900

    [fix] Search slice import 경로변경

--- src/redux/modules/searchSlice.js
+++ src/redux/modules/searchSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk, current } from "@reduxjs/toolkit";
 import { instance } from "../../shared/instance";
 
+
 export const getList = createAsyncThunk("GET_LIST", async (search) => {
   const response = await instance.get(`collections?keyword=${search}&offset=0&limit=50`);
   return response.data.data;
