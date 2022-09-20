e4036306a046d87d1659b67f94ecc9267dc2d1cc
jhhyung <jinhahyung@gmail.com>
2022-09-20 12:13:42 +0900

    [edit]카테고리슬라이스경로수정

--- src/components/features/addCollection/AddCollectionForm.js
+++ src/components/features/addCollection/AddCollectionForm.js
@@ -2,10 +2,8 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getCategory,
-  postCollection,
-} from "../../../redux/modules/collectionSlice";
+import { postCollection } from "../../../redux/modules/collectionSlice";
+import { getCategory } from "../../../redux/modules/categorySlice";
 import useInputs from "../../hooks/useInput";
 import icon_backspace_black from "../../../shared/svg/icon_backspace.svg";
 import icon_add from "../../../shared/svg/icon_add.svg";
@@ -13,9 +11,7 @@ import icon_add from "../../../shared/svg/icon_add.svg";
 const AddCollectionForm = () => {
   const nav = useNavigate();
   const dispatch = useDispatch();
-  const categories = useSelector(
-    (state) => state.myCollectionSlice.category.data
-  );
+  const categories = useSelector((state) => state.categorySlice.category.data);
   const videoList = useSelector((state) => state.myCollectionSlice.videoList);
   useEffect(() => {
     dispatch(getCategory());
--- src/components/features/mainList/MainListWrap.js
+++ src/components/features/mainList/MainListWrap.js
@@ -1,23 +1,45 @@
 import React from "react";
-import styled from "styled-components";
 import TodaysWeatherCollections from "./TodaysWeatherCollections";
 import RecommendedCollections from "./RecommendedCollections";
 import FamousCollections from "./FamousCollections";
 import RecentCollections from "./RecentCollections";
 import SearchWrap from "../search/SearchWrap";
 import RecommendedCategory from "./RecommendedCategory";
-
+import Carousel from "../../common/Carousel";
+import CarouselItem from "../../common/CarouselItem";
 const MainListWrap = () => {
   const todaysWeatherCategoryId = "631e7d7a4ae4c133c405a965"; // 오늘 날씨에 추천하는
   const recommendedCategoryId = "631e7d7a4ae4c133c405a966"; // 바로 지금 추천하는
   const famousCategoryId = "6319aeebd1e330e86bbade9f"; // 인기있는
   const recentCategoryId = "631e7d7a4ae4c133c405a964"; // 새로운
 
+  const sample = [
+    "https://via.placeholder.com/320x180",
+    "https://via.placeholder.com/320x180",
+    "https://via.placeholder.com/320x180",
+    "https://via.placeholder.com/320x180",
+    "https://via.placeholder.com/320x180",
+    "https://via.placeholder.com/320x180",
+  ];
   return (
     <>
-      <TodaysWeatherCollections categoryId={todaysWeatherCategoryId} />
+      <Carousel
+        slidesToShow={1}
+        centerMode={true}
+        infinite={true}
+        speed={500}
+        centerPadding={"40px"}
+        autoPlay={true}
+        className={"center"}
+      >
+        {sample?.map((data, idx) => (
+          <CarouselItem key={idx} src={data} alt={idx} />
+        ))}
+      </Carousel>
       <SearchWrap />
       <RecommendedCategory />
+
+      <TodaysWeatherCollections categoryId={todaysWeatherCategoryId} />
       <RecommendedCollections categoryId={recommendedCategoryId} />
       <FamousCollections categoryId={famousCategoryId} />
       <RecentCollections categoryId={recentCategoryId} />
