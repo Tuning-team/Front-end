d56990b1c44e54792abb773944952b249eb1bb9c
C.HYO (Dopest-HYO) <hyo01_@naver.com>
2022-09-25 15:38:55 +0900

    [edit] search CSS 수정 완료

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import SearchWrap from "../components/features/search/SearchWrap";
 import Navbar from "../components/common/Navbar";
 import Layout from "../components/common/Layout";
+import SearchHeaders from "../components/features/search/SearchHeader";
 import StickyButtons from "../components/common/StickyButtons";
 
 const SearchPage = () => {
   return (
     <Layout>
+      <SearchHeaders />
       <SearchWrap />
       <StickyButtons />
       <Navbar />
