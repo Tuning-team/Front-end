09b52888331638f4ebf10138df7440784cb53862
jhhyung <jinhahyung@gmail.com>
2022-09-28 01:19:42 +0900

    [edit]폰트추가/globalstyled-reset적용/홍보문구적용

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,20 +1,50 @@
 import React from "react";
 import styled from "styled-components";
-
+import tuningLogo from "../../shared/svg/icon_welcome.svg";
 const Layout = ({ children }) => {
-  return <StyleLayout>{children}</StyleLayout>;
+  return (
+    <>
+      <LogoContainer>
+        <img src={tuningLogo} alt="tuningLogo" />
+        <h1>내가 모은 영상 플레이 리스트를 내 방식대로 편하게 보관하자!</h1>
+      </LogoContainer>
+      <Line></Line>
+      <StyleLayout>{children}</StyleLayout>
+    </>
+  );
 };
 export default Layout;
-
 const StyleLayout = styled.div`
-  max-width: 360px;
-  /* min-width: 360px; */
-  /* border: 1px solid green; */
-  /* padding-bottom: 5rem; */
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
   position: relative;
-  // align-items: center;
+
+  max-width: 360px;
+  min-width: 360px;
+  background-color: #fff;
+`;
+const LogoContainer = styled.div`
+  position: fixed;
+  top: 48%;
+  right: 50%;
+  transform: translate(-50%, -48%);
+  width: 20%;
+  & img {
+    width: 50%;
+    height: auto;
+  }
+  & h1 {
+    font-family: "Noto Sans KR";
+    font-size: 200%;
+    font-weight: 500;
+    letter-spacing: -0.7px;
+    color: #505050;
+  }
+`;
+const Line = styled.div`
+  background-color: var(--color-primary);
+  height: 0.313rem;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 39%;
+  z-index: -1;
 `;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { CookiesProvider } from "react-cookie";
+// import { CookiesProvider } from "react-cookie";
 import configstore from "./redux/configStore";
 import { Provider } from "react-redux";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import { GlobalStyle } from "./shared/GlobalStyle";
 const root = ReactDOM.createRoot(document.getElementById("root"));
