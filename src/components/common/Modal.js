cd12883ec7220dae387af22574eed0355aa9f37f
jhhyung <jinhahyung@gmail.com>
2022-09-27 18:44:37 +0900

    [edit]모달 로직 변경

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,16 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-
-const Modal = ({ children, setModal }) => {
+import { ReactComponent as CloseIcon } from "../../shared/svg/icon_close.svg";
+const Modal = (props) => {
   const closeModal = () => {
-    setModal((prev) => !prev);
+    props.setModal((prev) => !prev);
   };
 
   return (
     <ModalBackground onClick={closeModal}>
       <ModalContainer onClick={(e) => e.stopPropagation()}>
-        <CloseModalButton onClick={closeModal}>&times;</CloseModalButton>
-        <ModalDesc>{children}</ModalDesc>
+        {props.modalKeyword && (
+          <ModalKeyword>{props.modalKeyword}</ModalKeyword>
+        )}
+        <CloseModalButton onClick={closeModal}>
+          <StyleCloseIcon />
+        </CloseModalButton>
+        <ModalDesc margin={props.margin}>{props.children}</ModalDesc>
       </ModalContainer>
     </ModalBackground>
   );
@@ -37,23 +42,33 @@ const ModalContainer = styled.div`
   display: block;
   padding: 1rem;
   background-color: white;
-  border-radius: 6px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.16);
+  border-radius: 16px;
   user-select: none;
   width: 18.5rem;
 `;
-
+const ModalKeyword = styled.h2`
+  margin: 0;
+  font-size: 1.125rem;
+  font-weight: bold;
+  letter-spacing: -0.9px;
+  text-align: left;
+  padding-bottom: 0.25rem;
+  border-bottom: 1px solid #f5f5f5;
+`;
 const CloseModalButton = styled.div`
   position: absolute;
-  right: 10px;
-  top: 6px;
-  display: inline-block;
-  background: transparent;
-  border: none;
-  font-size: 30px;
+  right: 0.5rem;
+  top: 0.5rem;
   cursor: pointer;
 `;
-
+const StyleCloseIcon = styled(CloseIcon)`
+  box-sizing: border-box;
+  width: 2.5rem;
+  height: 2.5rem;
+  padding: 0.5rem;
+`;
 const ModalDesc = styled.div`
   display: block;
-  margin: 2rem auto;
+  margin: ${(props) => props.margin || "2rem auto"};
 `;
