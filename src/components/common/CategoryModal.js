20cee99e899f99e347a7382ef2ebab240dccac26
jhhyung <jinhahyung@gmail.com>
2022-09-29 05:37:22 +0900

    [edit]카테고리모달 반응형 완료

--- src/components/common/CategoryModal.js
+++ src/components/common/CategoryModal.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCategory } from "../../redux/modules/categorySlice";
 import styled from "styled-components";
 import { keyframes } from "styled-components";
-import icon_close from "../../shared/svg/24_ena_close.svg";
+import { ReactComponent as CloseIcon } from "../../shared/svg/24_ena_close.svg";
 
 const CategoryModal = ({ setIsCategoryShown }) => {
   const nav = useNavigate();
@@ -18,31 +18,26 @@ const CategoryModal = ({ setIsCategoryShown }) => {
 
   return (
     <ModalFloater setIsCategoryShown="setIsCategoryShown">
-      <ContentsWrap style={{ margin: "1rem" }}>
-        <TitleWrap>
-          <Title>카테고리</Title>
-          <Icon
-            onClick={() => setIsCategoryShown((prev) => !prev)}
-            src={icon_close}
-          ></Icon>
-        </TitleWrap>
+      <TitleWrap>
+        <Title>카테고리</Title>
+        <StyleCloseIcon onClick={() => setIsCategoryShown((prev) => !prev)} />
+      </TitleWrap>
 
-        <ul>
-          {categories?.map((elem) => {
-            return (
-              <Li
-                key={elem._id}
-                onClick={() => {
-                  nav(`/category/${elem._id}`);
-                  setIsCategoryShown((prev) => !prev);
-                }}
-              >
-                {elem.categoryName}
-              </Li>
-            );
-          })}
-        </ul>
-      </ContentsWrap>
+      <ul>
+        {categories?.map((elem) => {
+          return (
+            <Li
+              key={elem._id}
+              onClick={() => {
+                nav(`/category/${elem._id}`);
+                setIsCategoryShown((prev) => !prev);
+              }}
+            >
+              {elem.categoryName}
+            </Li>
+          );
+        })}
+      </ul>
     </ModalFloater>
   );
 };
@@ -53,71 +48,55 @@ from{
   transform: translateX(-150px)
 }
 to{
-  transform: translateX(0px)
+  transform: translateX(0)
 }
 `;
 
 const ModalFloater = styled.div`
-  display: flex;
+  background-color: white;
+  padding: 0 1.25rem;
   position: absolute;
   top: 0;
-  z-index: 200;
-  background-color: white !important;
   width: 100%;
-  // height: 100%;
-  left: ${(props) => (props.setIsCategoryShown ? "0" : "-100%")};
+  min-height: 100vh;
+  z-index: 500;
 
-  animation-duration: 0.3s;
-  animation-timing-function: ease-out;
+  /* 슬라이드 애니메이션 적용 */
   animation-name: ${slideIn};
+  animation-duration: 0.5s;
+  animation-timing-function: ease-in-out;
   animation-fill-mode: forwards;
-  /*테스트*/
-  height: 100%;
-  overflow: scroll;
-  /* 스크롤바 숨겨주는 css */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  position: fixed;
-  top: 0;
-  transition: visibility 300ms, left 300ms ease-in;
-  width: calc(100% -3px);
-  will-change: left;
-  z-index: 1002;
-`;
-const ContentsWrap = styled.div`
-  width: 22rem;
 `;
 const TitleWrap = styled.div`
+  position: sticky;
+  top: 0;
+  padding-top: 2.5rem;
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid #eeeeee;
+  margin-bottom: 1rem;
+  background-color: white;
 `;
 
 const Title = styled.h1`
-  display: block;
-  font-size: 1.5rem;
+  font-size: 2rem;
   font-weight: bold;
-  line-height: 0.75;
   letter-spacing: -1.2px;
-  text-align: left;
+`;
+const StyleCloseIcon = styled(CloseIcon)`
+  width: 3rem;
+  height: 3rem;
+  padding: 0.5rem;
 `;
 
-const Icon = styled.img`
-  width: 1.5rem;
-  height: 1.5rem;
-`;
 const Li = styled.li`
-  margin: 1rem auto;
-  font-size: 1.125rem;
+  font-size: 1.25rem;
   font-weight: bold;
   letter-spacing: -0.9px;
-  text-align: left;
-  padding: 0.5rem;
-  border-bottom: 1px solid #eeeeee;
+  padding: 1rem 0;
+  border-top: 1px solid #eeeeee;
   cursor: pointer;
   &:active {
-    background-color: white;
+    background-color: #eeeeee;
     opacity: 1;
     font-weight: 900;
   }
