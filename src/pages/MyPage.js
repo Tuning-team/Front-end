16e3af0d191f4659a048a9c004f55799021b96b1
MinjiJeon <minji799@gmail.com>
2022-09-29 12:55:38 +0900

    [fix]mypage 불필요한 코드 삭제

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,6 +1,5 @@
 import MyPageWrap from "../components/features/user/MyPageWrap";
 import Navbar from "../components/common/Navbar";
-import Headers from "../components/common/Headers";
 import Layout from "../components/common/Layout";
 import StickyButtons from "../components/common/StickyButtons";
 import UserInfo from "../components/features/user/UserInfo";
