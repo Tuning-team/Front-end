b77d4afdda9201e867661025f89741b36b3d9ab3
MinjiJeon <minji799@gmail.com>
2022-10-02 16:00:38 +0900

    [add]workbox 설정

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,80 +1,81 @@
-// fetch event
-self.addEventListener("fetch", (evt) => {
-  // check if request is made by chrome extensions or web page
-  // if request is made for web page url must contains http.
-  if (!evt.request.url.startsWith("http"))
-    // skip the request. if request is not made with http protocol
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
-    evt.respondWith(
-      caches
-        .match(evt.request)
-        .then(
-          (cacheRes) =>
-            cacheRes ||
-            fetch(evt.request).then((fetchRes) =>
-              caches.open(dynamicNames).then((cache) => {
-                cache.put(evt.request.url, fetchRes.clone());
-                // check cached items size
-                limitCacheSize(dynamicNames, 75);
-                return fetchRes;
-              })
-            )
-        )
-        .catch(() => caches.match("/fallback"))
-    );
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  "index.html",
+  "./", // Alias for index.html
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
 });
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then((cache) => {
-    cache.keys().then((keys) => {
-      if (keys.length > size) {
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
-// function registerValidSW(swUrl, config) {
-//   navigator.serviceWorker
-//     .register(swUrl)
-//     .then((registration) => {
-//       registration.onupdatefound = () => {
-//         const installingWorker = registration.installing;
-//         if (installingWorker == null) {
-//           return;
-//         }
-//         installingWorker.onstatechange = () => {
-//           if (installingWorker.state === "installed") {
-//             if (navigator.serviceWorker.controller) {
-//               // At this point, the updated precached content has been fetched,
-//               // but the previous service worker will still serve the older
-//               // content until all client tabs are closed.
-//               console.log(
-//                 "New content is available and will be used when all " +
-//                   "tabs for this page are closed. See https://bit.ly/CRA-PWA."
-//               );
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener("fetch", (event) => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-//               // Execute callback
-//               if (config && config.onUpdate) {
-//                 config.onUpdate(registration);
-//               }
-//             } else {
-//               // At this point, everything has been precached.
-//               // It's the perfect time to display a
-//               // "Content is cached for offline use." message.
-//               console.log("Content is cached for offline use.");
-
-//               // Execute callback
-//               if (config && config.onSuccess) {
-//                 config.onSuccess(registration);
-//               }
-//             }
-//           }
-//         };
-//       };
-//     })
-//     .catch((error) => {
-//       console.error("Error during service worker registration:", error);
-//     });
-// }
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
