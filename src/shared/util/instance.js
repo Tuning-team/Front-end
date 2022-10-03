43f33a3f705511dbdd09eb89aa28435a82a214cb
MinjiJeon <minji799@gmail.com>
2022-10-04 02:03:49 +0900

    [fix]cicd수정

--- src/shared/util/instance.js
+++ src/shared/util/instance.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 
 export const instance = axios.create({
-  baseURL: "https://api.tube-tuning.com/api",
+  baseURL: "https://tube-tuning.com/api",
   headers: { authorization: `Bearer ${getCookie("token")}` },
 });
