9736ecf06a14d32c5320a53351cb3cbe98a61782
MinjiJeon <minji799@gmail.com>
2022-10-04 17:22:40 +0900

    [edit]서비스워커수정, youtubevideo수정

--- src/components/features/collection/YoutubePlayer.js
+++ src/components/features/collection/YoutubePlayer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import ReactPlayer from "react-player/youtube";
@@ -7,8 +7,8 @@ const YoutubePlayer = () => {
   const videoId = useSelector((state) => state.collectionSlice.selectedVideoId);
   const videoList = useSelector((state) => state.collectionSlice.videos);
   const defaultVideoId = videoList[0]?.videoId;
-  const baseUrl = "https://www.youtube.com/watch?v=";
-
+  const baseUrl = "http://www.youtube.com/watch?v=";
+  const id = videoList.map((x) => x.videoId);
   const youtubeConfig = {
     youtube: {
       playerVars: {
@@ -18,47 +18,39 @@ const YoutubePlayer = () => {
       },
     },
   };
+  //!비디오 클릭시
+  const [test, setTest] = useState(defaultVideoId);
+  useEffect(() => {
+    setTest(videoId);
+  }, [videoId]);
+  //!디폴트비디오
+  useEffect(() => {
+    !defaultVideoId && setTest(defaultVideoId);
+  }, [defaultVideoId]);
+
   return (
     <>
-      {videoId ? (
-        videoList?.map((elem) => {
-          if (elem.videoId === videoId) {
-            return (
-              <PlayerWrapper key={elem._id}>
-                <ReactPlayer
-                  url={baseUrl + elem.videoId}
-                  className="player"
-                  light={true}
-                  width="100%"
-                  height="100%"
-                  playing={true}
-                  muted={true}
-                  volume={0.3}
-                  controls={true}
-                  playsinline={false}
-                  conifg={youtubeConfig}
-                />
-              </PlayerWrapper>
-            );
-          }
-        })
-      ) : (
-        <PlayerWrapper>
-          <ReactPlayer
-            url={baseUrl + defaultVideoId}
-            className="player"
-            light={true}
-            width="100%"
-            height="100%"
-            playing={true}
-            muted={true}
-            volume={0.3}
-            controls={true}
-            playsinline={false}
-            conifg={youtubeConfig}
-          />
-        </PlayerWrapper>
-      )}
+      <PlayerWrapper>
+        <ReactPlayer
+          // url={baseUrl + elem.videoId}
+          url={baseUrl + test}
+          className="player"
+          light={true}
+          width="100%"
+          height="100%"
+          playing={true}
+          muted={true}
+          volume={0.3}
+          controls={true}
+          playsinline={false}
+          conifg={youtubeConfig}
+          onEnded={() => {
+            const num = id.indexOf(test);
+            setTest(videoList[num + 1].videoId);
+          }}
+        />
+      </PlayerWrapper>
+      );
     </>
   );
 };
