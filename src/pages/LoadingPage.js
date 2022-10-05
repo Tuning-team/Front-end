62f20056dcfe02353b9abdb900c3b58e72d8eed0
jhhyung <jinhahyung@gmail.com>
2022-10-05 14:28:44 +0900

    [edit]route이동시 레이아웃컴포넌트 flickering해결

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/common/Layout";
+import Loading from "../components/common/Loading";
+
+const LoadingPage = () => {
+  return (
+    <Layout>
+      <Loading />
+    </Layout>
+  );
+};
+export default LoadingPage;
